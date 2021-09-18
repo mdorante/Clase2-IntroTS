8f2156be912d8671393fdb0e18e9263e0b31dfd4
mdorante <mdorante10@gmail.com>
2021-09-18 19:20:36 -0300

    add datos variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Soy la variable title de app.component.ts';
+
+  datos: Object = {
+    Nombre: 'Manuel',
+    Apellido: 'Dorante',
+    Edad: 25,
+    Documento: 95837491,
+  };
 }
