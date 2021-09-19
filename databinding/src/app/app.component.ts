49093626051a86c9acda122d729c7c94a2359c33
mdorante <mdorante10@gmail.com>
2021-09-18 23:24:10 -0300

    add persona object

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'databinding';
+
+  persona: Object = {
+    Nombre: 'Lionel',
+    Apellido: 'Messi',
+    Edad: 34,
+    Documento: 1234567890,
+  };
 }
