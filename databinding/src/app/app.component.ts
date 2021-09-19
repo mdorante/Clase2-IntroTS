7128231da5bcfd11f9cb38300446b978aff071c9
mdorante <mdorante10@gmail.com>
2021-09-18 23:39:36 -0300

    add function for event binding

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -8,10 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'databinding';
 
-  persona: Object = {
+  persona: any = {
     Nombre: 'Lionel',
     Apellido: 'Messi',
     Edad: 34,
     Documento: 1234567890,
   };
+
+  eventoBoton() {
+    console.log(
+      'Presionaste el boton y disparaste una funcion del app.component.ts'
+    );
+    console.dir(this.persona);
+  }
 }
